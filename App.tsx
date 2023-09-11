c397740ceab2b08762db510b53f065cfbcc5eeed
Yuhyeon0516 <rladbgus0516@naver.com>
2023-09-11 21:46:54 +0900

    Set stack navigation screen

--- App.tsx
+++ App.tsx
@@ -1,14 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
+import {RootStackParamList} from './src/types';
+import SignupScreen from './src/SignupScreen/SignupScreen';
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function Screens() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{headerShown: false}}>
+                <Stack.Screen name="Signup" component={SignupScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
 
 function App(): JSX.Element {
-    return <></>;
+    return <Screens />;
 }
 
 export default App;

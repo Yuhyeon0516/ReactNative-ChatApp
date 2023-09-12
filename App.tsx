d8441fd0f963ce8536ddfd9962e29f325f52561f
Yuhyeon0516 <rladbgus0516@naver.com>
2023-09-12 13:22:23 +0900

    Stacks were divided according to login status.

--- App.tsx
+++ App.tsx
@@ -1,19 +1,41 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React from 'react';
+import React, {useCallback, useContext} from 'react';
 import {RootStackParamList} from './src/types';
 import SignupScreen from './src/SignupScreen/SignupScreen';
 import AuthProvider from './src/components/AuthProvider';
 import SigninScreen from './src/SigninScreen/SigninScreen';
+import AuthContext from './src/components/AuthContext';
+import HomeScreen from './src/HomeScreen/HomeScreen';
+import LoadingScreen from './src/LoadingScreen/LoadingScreen';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function Screens() {
+    const {user, processingSignin, processingSignup, initialized} =
+        useContext(AuthContext);
+    const renderRootStack = useCallback(() => {
+        if (!initialized) {
+            return <Stack.Screen name="Loading" component={LoadingScreen} />;
+        }
+        if (user && !processingSignin && !processingSignup) {
+            // login
+            return <Stack.Screen name="Home" component={HomeScreen} />;
+        } else {
+            // logout
+            return (
+                <>
+                    <Stack.Screen name="Signup" component={SignupScreen} />
+                    <Stack.Screen name="Signin" component={SigninScreen} />
+                </>
+            );
+        }
+    }, [initialized, processingSignin, processingSignup, user]);
+
     return (
         <NavigationContainer>
             <Stack.Navigator screenOptions={{headerShown: false}}>
-                <Stack.Screen name="Signup" component={SignupScreen} />
-                <Stack.Screen name="Signin" component={SigninScreen} />
+                {renderRootStack()}
             </Stack.Navigator>
         </NavigationContainer>
     );
